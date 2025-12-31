82a4285e5052dea619f81ae37f27b0d1e95e7d14
Sai Ranjit Tummalapalli <sairanjit5@gmail.com>
2026-01-01 01:25:51 +0530

    feat: add did resolver cli and release workflow

--- src/bin/did-resolver-cli.ts
+++ src/bin/did-resolver-cli.ts
@@ -7,10 +7,17 @@ const program = new Command();
 program
   .name("did-resolver-cli")
   .description("DID Resolver CLI")
-  .version("0.0.1")
-  .argument("<name>", "Name to print")
-  .action((name: string) => {
-    console.log(name);
+  .version("0.0.2")
+  .argument("<did>", "DID to resolve using Universal Resolver (format: did:method:specific-id)")
+  .action(async (did: string) => {
+    try {
+      const response = await fetch(`https://dev.uniresolver.io/1.0/identifiers/${did}`)
+      const responseJson = await response.json()
+      console.log(JSON.stringify(responseJson, null, 2))
+    } catch (error) {
+      console.error("Error:", error)
+      process.exit(1)
+    }
   });
 
 program.parse();
